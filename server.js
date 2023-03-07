dbf1411276bc34fa8b446d52af41a10078809127
toainguyen1504 <toaigcs200874@fpt.edu.vn>
2023-03-08 02:45:15 +0700

    update 5

--- server.js
+++ server.js
@@ -1,72 +1,73 @@
 // server.js
 //console.log('May Node be with you')
 
-const express = require('express');
-const bodyParser= require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
-const connectionString = "mongodb+srv://toaicong1504:Tt123456@cluster0.x7o0krf.mongodb.net/?retryWrites=true&w=majority";
+const connectionString =
+  "mongodb+srv://toaicong1504:Tt123456@cluster0.x7o0krf.mongodb.net/?retryWrites=true&w=majority";
 
-// (0) CONNECT: server -> connect -> MongoDB Atlas 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-    .then(client => {
-        console.log('Connected to Database')
-        
-        // (1a) CREATE: client -> create -> database -> 'star-wars-quotes'
-        // -> create -> collection -> 'quotes'
-        const db = client.db('star-wars-quotes')
-        const quotesCollection = db.collection('quotes')
-        
-        // To tell Express to EJS as the template engine
-        app.set('view engine', 'ejs') 
-        
-        // Make sure you place body-parser before your CRUD handlers!
-        app.use(bodyParser.urlencoded({ extended: true }))
+// (0) CONNECT: server -> connect -> MongoDB Atlas
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log("Connected to Database");
 
-        // To make the 'public' folder accessible to the public
-        app.use(express.static('public'))
+    // (1a) CREATE: client -> create -> database -> 'star-wars-quotes'
+    // -> create -> collection -> 'quotes'
+    const db = client.db("star-wars-quotes");
+    const quotesCollection = db.collection("quotes");
 
-        // To teach the server to read JSON data 
-        app.use(bodyParser.json())
+    // To tell Express to EJS as the template engine
+    app.set("view engine", "ejs");
 
-        // (2) READ: client -> browser -> url 
-        // -> server -> '/' -> collection -> 'quotes' -> find() 
-        // -> results -> index.ejs -> client
-        app.get('/', (req, res) => {
-            db.collection('quotes').find().toArray()
-                .then(results => {
+    // Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-                    // results -> server -> console
-                    console.log(results)
-                    
-                    // results -> index.ejs -> client -> browser 
-                    // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
-                    res.render('index.ejs', { quotes: results })
-                })
-                .catch(/* ... */)
+    // To make the 'public' folder accessible to the public
+    app.use(express.static("public"));
+
+    // To teach the server to read JSON data
+    app.use(bodyParser.json());
+
+    // (2) READ: client -> browser -> url
+    // -> server -> '/' -> collection -> 'quotes' -> find()
+    // -> results -> index.ejs -> client
+    app.get("/", (req, res) => {
+      db.collection("quotes")
+        .find()
+        .toArray()
+        .then((results) => {
+          // results -> server -> console
+          console.log(results);
+
+          // results -> index.ejs -> client -> browser
+          // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
+          res.render("index.ejs", { quotes: results });
         })
+        .catch(/* ... */);
+    });
 
-        // (1b) CREATE: client -> index.ejs -> data -> SUBMIT 
-        // -> post -> '/quotes' -> collection -> insert -> result
-        app.post('/quotes', (req, res) => {
-            quotesCollection.insertOne(req.body)
-            .then(result => {
-                
-                // results -> server -> console
-                console.log(result)
+    // (1b) CREATE: client -> index.ejs -> data -> SUBMIT
+    // -> post -> '/quotes' -> collection -> insert -> result
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          // results -> server -> console
+          console.log(result);
 
-                // -> redirect -> '/'
-                res.redirect('/')
-             })
-            .catch(error => console.error(error))
+          // -> redirect -> '/'
+          res.redirect("/");
         })
-        
-        // server -> listen -> port -> 3000
-        app.listen(3000, function() {
-            console.log('listening on 3000')
-        })
-    })
+        .catch((error) => console.error(error));
+    });
 
-
+    // server -> listen -> port -> 3000
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
+  }
+);
